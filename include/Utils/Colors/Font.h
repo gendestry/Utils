15d47fb5fd49116f18d58d649bb075bf4279c588
gendestry <zan.oberstar@gmail.com>
2026-07-08 05:01:42 +0200

    font bugfix

--- include/Utils/Colors/Font.h
+++ include/Utils/Colors/Font.h
@@ -3,22 +3,25 @@
 //
 
 #pragma once
-#include <string>
 #include "RGB.h"
+#include <string>
 
 namespace Utils::Font
 {
-    constexpr std::string colorBlack = "\x1B[30m";
-    constexpr std::string colorRed = "\x1B[31m";
-    constexpr std::string colorGreen = "\x1B[32m";
-    constexpr std::string colorYellow = "\x1B[33m";
-    constexpr std::string colorBlue = "\x1B[34m";
-    constexpr std::string colorMagenta = "\x1B[35m";
+constexpr std::string colorBlack = "\x1B[30m";
+constexpr std::string colorRed = "\x1B[31m";
+constexpr std::string colorGreen = "\x1B[32m";
+constexpr std::string colorYellow = "\x1B[33m";
+constexpr std::string colorBlue = "\x1B[34m";
+constexpr std::string colorMagenta = "\x1B[35m";
 
-    constexpr std::string colorReset = "\x1B[0m";
-    constexpr std::string colorDim = "\x1B[2m";
-    constexpr std::string colorItalic = "\x1B[3m";
+constexpr std::string colorReset = "\x1B[0m";
+constexpr std::string colorDim = "\x1B[2m";
+constexpr std::string colorItalic = "\x1B[3m";
 
-    std::string colorByRGB(int r, int g, int b, bool fg = true);
-    std::string colorByRGB(const Colors::RGB& color, bool fg = true);
-}
+constexpr std::string reset = "\x1B[0m";
+constexpr std::string bold = "\x1B[1m";
+
+std::string colorByRGB(int r, int g, int b, bool fg = true);
+std::string colorByRGB(const Colors::RGB &color, bool fg = true);
+} // namespace Utils::Font
