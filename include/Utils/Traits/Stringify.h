03dd7f44a1c3c545dd04f8eb8e36b06515f75176
gendestry <zan.oberstar@gmail.com>
2026-08-20 00:38:23 +0200

    stringify

--- include/Utils/Traits/Stringify.h
+++ include/Utils/Traits/Stringify.h
@@ -11,6 +11,6 @@ namespace Utils::Traits
 struct Stringify
 {
     virtual ~Stringify() = default;
-    virtual std::string toString() = 0;
+    virtual std::string toString() const = 0;
 };
 } // namespace Utils::Traits
